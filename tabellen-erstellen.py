6b6cfecfd41899c78a9c523fdf1258e89cb4764a
meisterk <franz.kohnle@sabel.com>
2020-10-08 12:09:55 +0200

    Add schueler

--- tabellen-erstellen.py
+++ tabellen-erstellen.py
@@ -4,11 +4,24 @@ cursur = connection.cursor()
 
 # Tabelle klasse erstellen
 sql = """
-CREATE TABLE klasse (
+CREATE TABLE IF NOT EXISTS klasse (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT
 )
 """
 cursur.execute(sql)
+
+# Tabelle schueler erstellen
+sql = """
+CREATE TABLE  IF NOT EXISTS schueler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    vorname TEXT,
+    nachname TEXT,
+    klasse_id INTEGER,
+    FOREIGN KEY (klasse_id) REFERENCES klasse(id)
+)
+"""
+cursur.execute(sql)
+
 connection.commit()
 connection.close()
